1829ef77978313475955a4eda08e5e2566de75f8
Italo souza <nos.100@hotmail.com>
2022-03-09 15:29:49 -0300

    CSS

---
+++ Modulo1/html-sistema-loja/fixacaoJS_2.md
@@ -0,0 +1,14 @@
+```function calculaPrecoTotal(quantidade) {
+    // Escreva seu código aqui
+    
+    if(quantidade < 12) {
+    return quantidade * 1.30
+  }else if(quantidade => 12){
+   
+          return quantidade * 1.00
+  
+  }else{
+      return "error"
+  }
+  
+  }```
---
+++ Modulo1/html-sistema-loja/index.js
@@ -0,0 +1,20 @@
+//let quantidade = 12
+
+function calculaPrecoTotal(quantidade) {
+    // Escreva seu código aqui
+    
+    if(quantidade < 12) {
+    return quantidade * 1.30
+  }else if(quantidade => 12){
+   
+          return quantidade * 1.00
+  
+  }else{
+      return "error"
+  }
+  
+  }
+  //console.log(calculaPrecoTotal(quantidade))
+
+
+  //TESTE EXTERNO PARA ANALISE
