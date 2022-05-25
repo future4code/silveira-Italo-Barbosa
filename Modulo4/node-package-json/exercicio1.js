05cf4a2c21ea3a591a2c19c030128b4916de49bf
italosouza09 <nos.100@hotmail.com>
2022-05-25 15:04:21 -0300

    Delete exercicio1.js

--- Modulo4/node-package-json/exercicio1.js
+++
@@ -1,9 +0,0 @@
-
-const name =  "Italo"
-const idade = Number(16)
-
-console.log (`Olá, ${name}! você tem ${idade}`)
-
-const anos = idade + 7
-
-console.log(`Olá, ${name}! Você tem ${idade} anos. Em sete anos você terá ${anos}`)
