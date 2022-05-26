1c638cb003869edd328e2229be1013ebc969ebdf
Italo souza <nos.100@hotmail.com>
2022-05-26 14:58:52 -0300

    aprofundamento-typescript

---
+++ Modulo3/aprofundamento-typescript/src/exercicio1.ts
@@ -0,0 +1,72 @@
+
+
+let Name: string = "seila"
+// error
+let num: Number = 123
+
+const obj : {
+    nome: string,
+    idade: Number,
+    corFavorita: string,
+} =
+{
+nome: "seila",
+idade: 14,
+corFavorita: "verde"
+
+
+}
+const obj2 : {
+    nome: string,
+    idade: Number,
+    corFavorita: string,
+} =
+{
+nome: "sei",
+idade: 12,
+corFavorita: "vermelho"
+
+
+}
+const obj3 : {
+    nome: string,
+    idade: Number,
+    corFavorita: string,
+} =
+{
+nome: "la",
+idade: 15,
+corFavorita: "azul"
+
+
+}
+const obj4 : {
+    nome: string,
+    idade: Number,
+    corFavorita: string,
+} =
+{
+nome: "ila",
+idade: 17,
+corFavorita: "amarelo"
+
+
+}
+
+
+type pessoa = {
+  
+    nome: "seila",
+    idade: 12,
+    corFavorita: "verde"
+
+}
+
+enum corFavorita {
+    verde = "verde",
+    amarelo = "amarelo",
+    azul = "azul",
+    vermelho = "vermelho",
+
+}
+
